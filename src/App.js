439832a145ee9e04b298b1da2fb5b32bd96c5e0c
cobbie <evelinagonda@gmail.com>
2017-07-22 21:10:26 +0200

    add dialog && rm unused button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import TButton from './components/t-button/t-button.component.js';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import TDialog from './components/t-dialog/t-dialog.component.jsx';
 
-
+injectTapEventPlugin();
 class App extends Component {
+  
   render() {
     return (
       <MuiThemeProvider>
         <div>
           <h2>Welcome to React</h2>
-          <TButton />
+          <TDialog />
         </div>
-
       </MuiThemeProvider>
     );
   }
